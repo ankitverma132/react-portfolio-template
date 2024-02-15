40a072a6a02b6c23e638d6b178d23232d413574c
Ankit <ankitv.ashokaec19@gmail.com>
2024-02-15 20:18:16 +0530

    Electronis world customization

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,7 +16,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            <Button type="primary">Call us at : +91 8738888868</Button>
             <div className="mt-10">
               <Socials />
             </div>
@@ -25,7 +25,7 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://tinyurl.com/Ankit132">
+        <Link href="https://ankit-verma132.netlify.app/">
           <a className="underline underline-offset-1">Ankit Verma</a>
         </Link>
       </h1>
